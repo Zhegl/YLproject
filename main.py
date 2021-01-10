ba80e60783934ce667971e0394cad955bafb2a88
zhegl <zhegl123@gmail.com>
2021-01-10 14:36:26 +0300

    Добавленны противники-бот, сделанны столкновения и поподания при стрельбе.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+from random import randint, choice
 
 clock = pygame.time.Clock()
 
@@ -11,7 +12,11 @@ player = None
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-
+alive_tanks = 0
+def gameover():
+    pass
+def win():
+    pass
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('textures', name)
@@ -36,7 +41,20 @@ def generate_level(level):
             elif level[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
+            elif level[y][x] == '*':
+                Tile('empty', x, y)
+                enemys.append(Enemy(x, y, 3))
+                global alive_tanks
+                alive_tanks += 1
+            elif level[y][x] == '/':
+                Tile('empty', x, y)
+                enemys.append(Enemy(x, y, 10))
+                alive_tanks += 1
+            elif level[y][x] == '&':
+                Tile('empty', x, y)
+                enemys.append(Enemy(x, y, 5))
+                alive_tanks += 1
+                # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
 
@@ -47,8 +65,14 @@ tile_images = {
 }
 player_image = load_image('test_tank_0.png')
 
-tile_width = tile_height = 50
 
+bullet_image = load_image('bullet.png')
+tile_width = tile_height = 50
+enemys = []
+collisions = []
+player_bullets = 0
+pl_xp = 5
+turn = 990
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type=None, pos_x=None, pos_y=None):
@@ -60,6 +84,7 @@ class Tile(pygame.sprite.Sprite):
                     tile_width * pos_x, tile_height * pos_y)
             else:
                 super().__init__(tiles_group, all_sprites)
+                collisions.append(CollisionBullet(pos_x, pos_y))
                 self.image = tile_images[tile_type]
                 self.rect = self.image.get_rect().move(
                     tile_width * pos_x, tile_height * pos_y)
@@ -71,12 +96,134 @@ class Player(pygame.sprite.Sprite):
         self.image = player_image
         self.rect = self.image.get_rect().move(pos_x, pos_y)
 
-
     def update(self):
         if pygame.sprite.spritecollideany(self, all_sprites):
             return False
         else:
             return True
+
+    def check(self):
+
+        for i in range(len(bullets) - 1, -1, -1):
+
+            el = bullets[i]
+            if pl_x < el.x < pl_x + 50 and pl_y < el.y < pl_y + 50 and not (el.isplayer):
+                del bullets[i]
+                global pl_xp
+                pl_xp -= 1
+            if pl_xp <= 0:
+                gameover()
+
+
+class CollisionBullet:
+    def __init__(self, pos_x=None, pos_y=None):
+        self.pos_x = pos_x * 50
+        self.pos_y = pos_y * 50
+
+    def check(self):
+        for i in range(len(bullets) - 1, -1, -1):
+            el = bullets[i]
+            if self.pos_x < el.x < self.pos_x + 50 and self.pos_y < el.y < self.pos_y + 50:
+                if el.isplayer:
+                    global player_bullets
+                    player_bullets -= 1
+                del bullets[i]
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos_x=None, pos_y=None, type=3):
+        super().__init__(player_group)
+        self.hp = type
+        self.type = str(type)
+
+        self.image = load_image('enemy_tank_0_' + self.type + '.png')
+        self.pos_x = pos_x * 50
+        self.pos_y = pos_y * 50
+
+        self.alivee = True
+        self.last_der = -1
+
+    def shoot(self):
+        bullets.append(Snaryad(round(self.pos_x + 50 // 2),
+                               round(self.pos_y + 50 // 2), 3, (0, 0, 0), self.last_der, False))
+
+    def check(self):
+        for i in range(len(bullets) - 1, -1, -1):
+            el = bullets[i]
+            if self.pos_x < el.x < self.pos_x + 50 and self.pos_y < el.y < self.pos_y + 50 and el.isplayer:
+                self.hp -= 1
+                del bullets[i]
+                global player_bullets
+                player_bullets -= 1
+        if self.hp <= 0:
+            self.alivee = False
+            self.pos_y = 1000
+            self.pos_x = 1000
+            global alive_tanks
+            alive_tanks -= 1
+
+    def move(self):
+        if randint(0, 1000) > 900:
+            if self.last_der == -2 and self.pos_y > pl_y and abs(self.pos_x - pl_x) <= 50:
+                self.shoot()
+            if self.last_der == 2 and self.pos_y < pl_y and abs(self.pos_x - pl_x) <= 50:
+                self.shoot()
+            if self.last_der == -1 and self.pos_x > pl_x and abs(self.pos_y - pl_y) <= 50:
+                self.shoot()
+            if self.last_der == 1 and self.pos_x < pl_x and abs(self.pos_y - pl_y) <= 50:
+                self.shoot()
+
+        if self.update() and randint(0, 1000) < turn:
+            if self.last_der == -2:
+                self.pos_y -= 1
+            if self.last_der == 2:
+                self.pos_y += 1
+            if self.last_der == -1:
+                self.pos_x -= 1
+            if self.last_der == 1:
+                self.pos_x += 1
+
+        else:
+            if self.last_der == -2:
+                self.last_der = 2
+            elif self.last_der == 2:
+                self.last_der = -2
+            elif self.last_der == -1:
+                self.last_der = 1
+            elif self.last_der == 1:
+                self.last_der = -1
+            if self.last_der == -2:
+                self.pos_y -= 1
+            if self.last_der == 2:
+                self.pos_y += 1
+            if self.last_der == -1:
+                self.pos_x -= 1
+            if self.last_der == 1:
+                self.pos_x += 1
+        if randint(0, 1000) > 995:
+            self.last_der = choice([1, -1, -2, 2])
+
+    def update(self):
+        self.rect = self.image.get_rect().move(self.pos_x, self.pos_y)
+        if pygame.sprite.spritecollideany(self, all_sprites):
+            return False
+        else:
+            return True
+
+    def draw(self):
+        self.move()
+        if self.last_der == -2:
+            self.image = load_image('enemy_tank_0_' + self.type + '.png')
+        elif self.last_der == 1:
+            self.image = load_image('enemy_tank_90_' + self.type + '.png')
+        elif self.last_der == 2:
+            self.image = load_image('enemy_tank_180_' + self.type + '.png')
+        elif self.last_der == -1:
+            self.image = load_image('enemy_tank_270_' + self.type + '.png')
+
+        self.check()
+        if self.alivee:
+            screen.blit(self.image, (self.pos_x, self.pos_y))
 
 
 def load_level(filename):
@@ -96,16 +243,22 @@ lastMove = 'up'
 
 
 class Snaryad():
-    def __init__(self, x, y, radius, color, direction):
+
+    def __init__(self, x, y, radius, color, direction, isplayer):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
         self.direction = direction
         self.vel = 5 * direction
+        self.image = bullet_image
+        self.rect = self.image.get_rect().move(x, y)
+        self.isplayer = isplayer
 
+    # def draw(self, win):
+    # pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
     def draw(self, win):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+        screen.blit(self.image, (self.x - 25, self.y - 25))
 
 
 if __name__ == '__main__':
@@ -123,6 +276,8 @@ if __name__ == '__main__':
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 running = False
+        if alive_tanks == 0:
+            win()
         proverka = Player(pl_x, pl_y)
 
         keys = pygame.key.get_pressed()
@@ -137,10 +292,11 @@ if __name__ == '__main__':
             elif lastMove == 'left':
                 direction = -1
 
-            if len(bullets) < 1:
+            if player_bullets < 1:
                 bullets.append(Snaryad(round(pl_x + 50 // 2),
                                        round(pl_y + 50 // 2), 3, (0, 0, 0),
-                                       direction))
+                                       direction, True))
+                player_bullets += 1
 
         for bullet in bullets:
             if bullet.direction == 1 or bullet.direction == -1:
@@ -233,11 +389,17 @@ if __name__ == '__main__':
                 pl_y -= speed
                 lastMove = 'up'
                 player_image = load_image('test_tank_0.png')
-
+        proverka.check()
+        for i in range(len(collisions)):
+            collisions[i].check()
         tiles_group.draw(screen)
+        for i in range(len(enemys)):
+            enemys[i].draw()
         screen.blit(player_image, (pl_x, pl_y))
+
         for bullet in bullets:
             bullet.draw(screen)
+
         clock.tick(fps)
         pygame.display.update()
 pygame.quit()
